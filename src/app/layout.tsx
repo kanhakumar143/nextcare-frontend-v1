e02ef230a8e0d48c2ae5988669d1a49fac22b78d
@kanhapro <kanhakumarkhatua2001@gmail.com>
2025-06-18 16:45:41 +0530

    feat: layout structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/layouts/Navbar";
+import SiteLayout from "@/components/layouts/SiteLayout";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,10 +26,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased px-70`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar />
-        {children}
+        <SiteLayout>{children}</SiteLayout>
       </body>
     </html>
   );
